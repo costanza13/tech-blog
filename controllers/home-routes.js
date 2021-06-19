3edae6865eca7edb1f551605530ddf28b3631262
mcc <costanza+git@gmail.com>
2021-06-19 14:24:28 -0700

    initial project set up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,6 @@ const sequelize = require('../config/connection');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('Breath... Encourage your teammates... Breath... Repeat...');
   
   res.render('homepage', {});
 });
