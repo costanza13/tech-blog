31603c23b8949410d4cefeced37f532e2e350b30
mcc <costanza+git@gmail.com>
2021-06-19 17:32:51 -0700

    added homepage template; show posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,35 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  
-  res.render('homepage', {});
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'body',
+      'created_at',
+      'updated_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+    ],
+    order: [['created_at', 'DESC'], ['id', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      console.log(req.session.loggedIn);
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
